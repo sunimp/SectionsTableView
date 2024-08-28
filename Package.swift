960c81ca238e35e523355aba02e879cbe38582d6
杨光 <yangguang@webull.com>
2024-08-28 10:08:30 +0800

    fix: typo

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ],
         dependencies: [
             .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-            .package(url: "https://github.com/sunimp/UIExtensions.Swift.git", .upToNextMajor(from: "1.2.0")),
+            .package(url: "https://github.com/sunimp/UIExtensions.Swift.git", .upToNextMajor(from: "1.2.1")),
             .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.54.0"),
         ],
         targets: [
